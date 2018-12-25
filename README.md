abf91ffec351306e7a2f32fe73fcd90eeb7794fe
jkalmar <janko.kalmar@gmail.com>
2018-12-25 15:46:48 +0100

    Readme && Added the script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# susemusic
+# SUSEmusic
 Bash script for downloading SUSE music videos from youtube using youtube-dl
+
+Before using please download [youtube-dl] and put it in your PATH (or install it using your distro package manager)
+
+[youtube-dl]: https://rg3.github.io/youtube-dl/
+
+
---
+++ SUSE_music_download.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+FORMAT="%(uploader)s%(title)s.%(ext)s"
+
+# Downloads SUSE music parody from youtube using youtube-dl utility
+
+# Dont forget - you got to be FREE
+
+# Coding in the Name of - (Rage Against the Machine Parody)
+youtube-dl --output $FORMAT -f '137+251'  https://www.youtube.com/watch?v=50Qs4gVHB_E&list=RDb0tsZB_LEQk&index=2
+
+# Code Together - (Come Together parody)
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=_9aLiv5M6AQ&index=3&list=RDb0tsZB_LEQk
+
+# Can't Stop the SUSE - (Can't Stop the Feeling parody)
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=A-Rn0iQEpc8&list=RDb0tsZB_LEQk&index=4
+
+# SUSE Now Hallelujah - A Musical Parody
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=-z8LDOujCJc&index=5&list=RDb0tsZB_LEQk
+
+# Linus Said - Music Parody (Momma Said)
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=oHNKTlz1lps&index=6&list=RDb0tsZB_LEQk
+
+# KUBERNETES - A SUSE Music Parody
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=0w6kXdHXxAA&list=RDb0tsZB_LEQk&index=7
+
+# Paint it Green - A SUSE Music Parody
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=b0tsZB_LEQk&list=RDb0tsZB_LEQk&index=1
+
+# SUSE Yes Please - Parody (Maroon 5 Sugar)
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=gWMQMPP8p3A&index=9&list=RDb0tsZB_LEQk
+
+# Uptime Funk - (Uptown Funk parody) 
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=SYRlTISvjww&index=10&list=RDb0tsZB_LEQk
+
+# What Does the Chameleon Say? (Ylvis - What Does the Fox Say parody)
+youtube-dl --output $FORMAT -f '137+251' https://www.youtube.com/watch?v=VNkDJk5_9eU&list=RDb0tsZB_LEQk&index=11
